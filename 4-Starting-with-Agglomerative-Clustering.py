dce81ab87f6e4085f2b3d333fb35150cdbad8a1a
Arfa Amine <arfa@srv-tp05>
2023-11-10 23:16:22 +0100

    amélioration partie 4

--- 4-Starting-with-Agglomerative-Clustering.py
+++ 4-Starting-with-Agglomerative-Clustering.py
@@ -2,122 +2,148 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 from scipy.io import arff
-from sklearn import cluster, metrics
-from sklearn.metrics import silhouette_score
-from sklearn.metrics.pairwise import euclidean_distances
+from sklearn import cluster
+from sklearn import metrics
 
-# Load data from an ARFF file
+def load_data(file_path):
+    databrut = arff.loadarff(open(file_path, 'r'))
+    return np.array([[x[0], x[1]] for x in databrut[0]])
+
+def plot_scatter(data, title):
+    f0, f1 = data[:, 0], data[:, 1]
+    plt.scatter(f0, f1, s=8)
+    plt.title(title)
+    plt.show()
+
+def plot_cluster(data, labels, title):
+    plt.scatter(data[:, 0], data[:, 1], c=labels, s=8)
+    plt.title(title)
+    plt.show()
+
+def agglomerative_clustering(data, linkage, n_clusters=None, distance_threshold=None):
+    model = cluster.AgglomerativeClustering(linkage=linkage, n_clusters=n_clusters, distance_threshold=distance_threshold)
+    model.fit(data)
+    labels = model.labels_
+    return labels
+
+def silhouette_analysis(data, max_clusters):
+    temps = []
+    scores = []
+
+    for d in range(2, max_clusters + 1):
+        tps1 = time.time()
+        labels = agglomerative_clustering(data, 'single', n_clusters=d)
+        tps2 = time.time()
+        temps.append(round((tps2 - tps1) * 1000, 2))
+        silhouette_score = metrics.silhouette_score(data, labels)
+        scores.append(silhouette_score)
+
+        # Imprimer les résultats à la console
+        print(f"Clusters: {d}, Temps: {round((tps2 - tps1) * 1000, 2)} ms, Score de Silhouette: {silhouette_score}")
+
+    plt.title("Évolution du score de silhouette")
+    plt.xlabel("nombre de clusters")
+    plt.ylabel("Score de silhouette")
+    plt.plot(range(2, max_clusters + 1), scores, 'g-o')
+    plt.show()
+
+# Exemple d'utilisation
 path = './artificial/'
-name = "long1.arff"
-databrut = arff.loadarff(open(path + str(name), 'r'))
-datanp = np.array([[x[0], x[1]] for x in databrut[0]])
+name = "target.arff"
+data = load_data(path + str(name))
 
-# Visualize the initial data
-f0 = datanp[:, 0]
-f1 = datanp[:, 1]
-plt.scatter(f0, f1, s=8)
-plt.title("Initial Data: " + str(name))
-plt.show()
+# Afficher les données initiales
+plot_scatter(data, "Données initiales : " + str(name))
 
-# Agglomerative Clustering with distance_threshold
-tps1 = time.time()
-seuil_dist = 10
-model = cluster.AgglomerativeClustering(distance_threshold=seuil_dist, linkage='average', n_clusters=None)
-model = model.fit(datanp)
-tps2 = time.time()
-labels = model.labels_
-k = model.n_clusters_
-leaves = model.n_leaves_
-plt.scatter(f0, f1, c=labels, s=8)
-plt.title("Agglomerative Clustering (average, distance_threshold= " + str(seuil_dist) + ") " + str(name))
-plt.show()
-print("Number of clusters =", k, ", Number of leaves =", leaves, " Runtime =", round((tps2 - tps1) * 1000, 2), "ms")
+# Appliquer l'analyse de silhouette et imprimer les résultats à la console
+silhouette_analysis(data, max_clusters=12)
 
-# Agglomerative Clustering with a fixed number of clusters
-k = 4
-tps1 = time.time()
-model = cluster.AgglomerativeClustering(linkage='average', n_clusters=k)
-model = model.fit(datanp)
-tps2 = time.time()
-labels = model.labels_
-kres = model.n_clusters_
-leaves = model.n_leaves_
-plt.scatter(f0, f1, c=labels, s=8)
-plt.title("Agglomerative Clustering (average, n_cluster= " + str(k) + ") " + str(name))
-plt.show()
-print("Number of clusters =", kres, ", Number of leaves =", leaves, " Runtime =", round((tps2 - tps1) * 1000, 2), "ms")
+# Exemple d'utilisation avec la meilleure valeur de silhouette (2 clusters)
+best_num_clusters = 4
+best_labels = agglomerative_clustering(data, 'single', n_clusters=best_num_clusters)
 
-# Silhouette Score Optimization
-linkage_method = 'average'
-dist_thresholds = np.linspace(1, 50, 50)
-best_score = -1
-best_threshold = None
+# Afficher le résultat du clustering
+plot_cluster(data, best_labels, f"Clustering Agglomératif avec {best_num_clusters} clusters")
 
-for dist_threshold in dist_thresholds:
-    model = cluster.AgglomerativeClustering(
-        distance_threshold=dist_threshold,
-        linkage=linkage_method,
-        n_clusters=None
-    )
-    model.fit(datanp)
-    labels = model.labels_
+def trouver_seuil_optimal(data, linkage, max_seuil=2.0, pas=0.1, metrique='silhouette'):
+    seuils = np.arange(0.1, max_seuil, pas)
+    meilleur_score = -1
+    meilleur_seuil = 0
 
-    # Check if there are at least two unique labels
-    unique_labels = np.unique(labels)
-    if len(unique_labels) > 1:
-        score = silhouette_score(datanp, labels)
-        if score > best_score:
-            best_score = score
-            best_threshold = dist_threshold
-    else:
-        print(f"Skipping iteration for distance threshold {dist_threshold} as only one unique label is present.")
+    for seuil in seuils:
+        etiquettes = agglomerative_clustering(data, linkage, distance_threshold=seuil)
 
-print(f"Best distance threshold for {linkage_method}: {best_threshold}")
-print(f"Silhouette Score: {best_score}")
+        if metrique == 'silhouette':
+            score = metrics.silhouette_score(data, etiquettes)
+        # Ajoutez d'autres métriques si nécessaire
 
-# Regroupement and Separation Scores
-model = cluster.AgglomerativeClustering(
-    distance_threshold=best_threshold,
-    linkage=linkage_method,
-    n_clusters=None
-)
+        if score > meilleur_score:
+            meilleur_score = score
+            meilleur_seuil = seuil
 
-def regroupement_score(X, labels):
-    pairwise_distances_matrix = euclidean_distances(X)
-    mask = labels[:, None] == labels
-    return pairwise_distances_matrix[mask].mean()
+    print(f"Meilleur score {metrique} : {meilleur_score} au seuil : {meilleur_seuil}")
 
-def separation_score(X, labels):
-    pairwise_distances_matrix = euclidean_distances(X)
-    mask = labels[:, None] != labels
-    return pairwise_distances_matrix[mask].mean()
+    # Vous pouvez retourner le meilleur seuil si nécessaire
+    return meilleur_seuil
 
-model.fit(datanp)
-labels = model.labels_
-regroupement = regroupement_score(datanp, labels)
-separation = separation_score(datanp, labels)
+# Exemple d'utilisation
+meilleur_seuil = trouver_seuil_optimal(data, linkage='single', max_seuil=2.0, pas=0.1, metrique='silhouette')
+print(f"Meilleur seuil : {meilleur_seuil}")
 
-print(f"Regroupement Score of the obtained solution: {regroupement:.3f}")
-print(f"Separation Score of the obtained solution: {separation:.3f}")
+##########################################################################
+# Fonction pour mesurer le temps de calcul du clustering agglomératif
+# pour différentes méthodes de linkage et un nombre variable de clusters.
+#
+# Paramètres :
+# - data : Les données à utiliser pour le clustering.
+# - max_clusters : Le nombre maximum de clusters à considérer.
+#
+# Méthode :
+# La fonction itère sur différentes méthodes de linkage ('single', 'complete', 'average', 'ward')
+# et mesure le temps de calcul du clustering agglomératif pour chaque nombre de clusters de 2 à max_clusters.
+# Les résultats sont affichés à la console et tracés dans une figure où l'axe x représente le nombre de clusters
+# et l'axe y représente le temps de calcul en millisecondes.
+#
+# Exemple d'utilisation :
+# mesure_temps_calcul(data, max_clusters=12)
+##########################################################################
 
-# Agglomerative Clustering with different linkage methods
-linkages = ["ward", "complete", "average", "single"]
-results = []
+def mesure_temps_calcul(data, max_clusters):
+    # Méthodes de linkage à considérer
+    methodes_linkage = ['single', 'complete', 'average', 'ward']
+    
+    for linkage in methodes_linkage:
+        temps = []
 
-for linkage_method in linkages:
-    tps1 = time.time()
-    model = cluster.AgglomerativeClustering(linkage=linkage_method, n_clusters=None, distance_threshold=10)
-    model = model.fit(datanp)
-    labels = model.labels_
-    score = silhouette_score(datanp, labels)
-    tps2 = time.time()
-    runtime = round((tps2 - tps1) * 1000, 2)
-    k = model.n_clusters_
-    leaves = model.n_leaves_
-    results.append((linkage_method, k, leaves, runtime, score))
+        # Itération sur différents nombres de clusters
+        for d in range(2, max_clusters + 1):
+            tps1 = time.time()
+            labels = agglomerative_clustering(data, linkage, n_clusters=d)
+            tps2 = time.time()
+            temps.append((tps2 - tps1) * 1000)  # temps en millisecondes
 
-# Display the results
-print(f"{'Linkage':<10} {'Number of Clusters':<12} {'Number of Leaves':<10} {'Runtime (ms)':<12} {'Silhouette Score':<17}")
-print('-' * 65)
-for res in results:
-    print(f"{res[0]:<10} {res[1]:<12} {res[2]:<10} {res[3]:<12.2f} {res[4]:<17.4f}")
+            # Imprimer les résultats à la console
+            print(f"Linkage: {linkage}, Clusters: {d}, Temps: {round((tps2 - tps1) * 1000, 2)} ms")
+
+        # Tracer la figure pour la méthode de linkage actuelle
+        plt.title(f"Temps de calcul pour la méthode de linkage '{linkage}'")
+        plt.xlabel("Nombre de clusters")
+        plt.ylabel("Temps de calcul (ms)")
+        plt.plot(range(2, max_clusters + 1), temps, 'g-o')
+        plt.show()
+
+# Exemple d'utilisation
+mesure_temps_calcul(data, max_clusters=20)
+
+# Appliquer l'analyse de silhouette avec la meilleure valeur
+best_labels = agglomerative_clustering(data, 'single', n_clusters=best_num_clusters)
+silhouette = metrics.silhouette_score(data, best_labels)
+davies_bouldin = metrics.davies_bouldin_score(data, best_labels)
+
+# Imprimer les scores à la console
+print(f"Silhouette Score : {silhouette}")
+print(f"Davies-Bouldin Score : {davies_bouldin}")
+
+# Afficher le résultat du clustering
+plot_cluster(data, best_labels, f"Clustering Agglomératif avec {best_num_clusters} clusters")
+
